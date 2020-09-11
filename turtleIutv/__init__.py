c8c10cc9c2d641360cbfa36309fe8acb7d3f7170
Mathieu Lacroix <lacroix@lipn.fr>
2020-09-11 16:34:51 +0200

    Change size canevas to 400

--- turtleIutv/__init__.py
+++ turtleIutv/__init__.py
@@ -20,7 +20,7 @@ class Turtle(widgets.DOMWidget):
     # thing on every sync
     points = List(sync=True)
 
-    SIZE = 800
+    SIZE = 400
     OFFSET = 20
     def __init__(self):
         '''Create a Turtle.
@@ -216,8 +216,8 @@ class Turtle(widgets.DOMWidget):
 
             t.home()
         '''
-        self.posX = 400
-        self.posY = 400
+        self.posX = 200
+        self.posY = 200
         if 90 < self.bearing <=270:
             self.b_change = - (self.bearing - 90)
         else:
